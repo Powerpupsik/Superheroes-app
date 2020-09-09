3aecac753a2c6a7b144c82a0fbdbb4a8f603566d
Opilane TTHK <opilane@tthk.ee>
2020-09-09 10:51:26 +0300

    fight

--- server.js
+++ server.js
@@ -1,6 +1,20 @@
  let superheroes = require('superheroes');
- console.log(superheroes.random());
+ let supervillains = require('supervillains');
+ //console.log(superheroes.random());
+
+ supervillains.all.forEach(supervillain => {
+    console.log(supervillain);
+});
+console.log(supervillains.random());
 
 superheroes.all.forEach(hero => {
     console.log(hero);
 });
+
+console.log(`${superheroes.random()} fights ${supervillains.random()}`);
+
+
+ 
+
+
+
